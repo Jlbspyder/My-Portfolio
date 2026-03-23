d716555c42994a967aa3257eee2fa35c4144f469
Debo Gbadebo <111382307+Jlbspyder@users.noreply.github.com>
2026-03-23 19:44:31 +0100

    Update image path for Typing Speed Test project

--- src/data.js
+++ src/data.js
@@ -35,7 +35,7 @@ const projects = [
   {
     id: 4,
     name: "Typing Speed Test",
-    image:  "/desktop-not-started.jpg",
+    image:  "/speed.jpg",
     liveDemo: true,
     privateCode: true,
     description: "Test your typing speed and accuracy.",
